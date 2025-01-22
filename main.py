298b21b6a5ac43c9861cab97734888801864acd1
Arian khan 21078101 <as8khan@uwaterloo.ca>
2025-01-22 11:28:11 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US State Game")
+screen.setup(width=700,height=700)
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
+
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
+
+guessed_states = []
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
+
+    if not answer_state or answer_state.lower() == "exit":
+        break
+
+    answer_state = answer_state.title()
+    if answer_state in data.state.values and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+
+        state_data = data[data.state == answer_state]
+        writer.goto(int(state_data.x), int(state_data.y))
+        writer.write(answer_state)
+
+
+screen.exitonclick()
